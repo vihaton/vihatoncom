3131be7e25a8fabac372156ff95747fe2097360e
Vili Hätönen <vili.hato@gmail.com>
2020-11-29 04:06:06 +0200

    import Material UI

--- vihatoncom/src/App.js
+++ vihatoncom/src/App.js
@@ -1,24 +1,28 @@
+import Container from '@material-ui/core/Container'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
+        </header>
+      </div>
+    </Container>
   );
 }
 
