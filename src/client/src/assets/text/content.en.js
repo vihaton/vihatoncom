b1c7093f31125d594cdd1a92bbd10919d3aa8313
Vili Hätönen <vili.hato@gmail.com>
2020-12-07 03:40:03 +0200

    move image assets to public; export their urls in text assets

---
+++ src/client/src/assets/text/content.en.js
@@ -0,0 +1,34 @@
+const content = {
+  sections: [
+    {
+      title: "Public Speaker",
+      content:
+        "Good ideas get better when they are shared. A good talk is not only inspiring, but it also leads to action: in order to change the world we must find and amplify the good and just ideas and make them the part of tomorrow's reality. Sharing is necessity for a sustainable change, hence I'm passionate about public speaking.",
+      image: {
+        src: "/img/ttt-finaali-vili-screen.jpg",
+        alt: "Vili on a stage",
+      },
+    },
+    {
+      title: "Data Scientist",
+      content:
+        "I design and build artificial intelligence (AI) systems for living. I've done research on both theoretical and applied machine learning (ML), and I'm always open for new ideas and projects which utilize ML to benefit humans and other living creatures.",
+      image: {
+        src: "/img/limo.jpeg",
+        alt:
+          "Image classification neural network classifies Vili as a limousine.",
+      },
+    },
+    {
+      title: "World Citizen",
+      content:
+        "My home is not a place. It is people. And there are wonderful, inspiring people all over the world. On my quest to learn more about this planet and its inhabitants I've visited over 45 countries, trying to grasp both the diversity of views and similarities of needs we express. I'm a meliorist; I believe that by working together we can make the world a better place.",
+      image: {
+        src: "/img/colombia-rio-verde-bus.jpg",
+        alt: "Vili sitting on top of a bus, plantations in the background.",
+      },
+    },
+  ],
+};
+
+export default content;
